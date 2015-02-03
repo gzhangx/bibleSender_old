38b9ee8ec6fae6aaca31f2ac468e5af3662098cb
Gang Zhang <gzhangx@hotmail.com>
2015-02-02 21:49:18 -0500

    fix accidental deletion

--- getdata.js
+++ getdata.js
@@ -316,7 +316,13 @@ var SendEmail = function(now)
         "type": "to"
     }];
 
-    fs.writeFileSync('debug.txt', data, 'utf8');
+    mandrill_client.messages.send({ "message": message, "async": async, "ip_pool": ip_pool}, function (result) {
+            console.log(result);
+        }, function (e) {
+        // Mandrill returns the error as an object with name and message keys
+        console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+        // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+    });
     return data;
 }
 
